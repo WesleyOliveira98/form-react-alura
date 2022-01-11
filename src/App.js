ae00f315e13668db885fc4afb57af0d34b681357
Wesley <wesleygabrieldeoliveira@gmail.com>
2022-01-10 22:46:32 -0300

    2. Utilizando o Material UI e 3. Forms e Hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import './App.css';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import { Container, Typography } from "@mui/material";
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
   return (
-    <Fragment>
-      <h1>Formulário de cadastro</h1>
+    <Container component="article" maxWidth="sm">
+      <Typography variant='h3' component='h1' align='center'>Formulário de cadastro</Typography>
       <FormularioCadastro />
-    </Fragment>
+    </Container>
   );
 }
 
--- src/components/FormularioCadastro/FormularioCadastro.js
+++ src/components/FormularioCadastro/FormularioCadastro.js
@@ -1,23 +1,53 @@
-import React from "react";
-import Button from '@mui/material/Button';
+import React, { useState } from "react";
+import { Button } from '@mui/material';
+import { TextField, Switch, FormControlLabel } from "@mui/material";
 
-function FormularioCadastro(){
+function FormularioCadastro() {
+    const [nome,setNome] = useState("");
+    const [sobrenome,setSobrenome] = useState("");
+
     return (
-        <form>
-            <label>Nome</label>
-            <input type="text" />
+        <form onSubmit={event => {
+            event.preventDefault();
+            console.log(nome, sobrenome);
+        }}>
+            <TextField
+                value={nome}
+                onChange={event => {
+                    let nomeDigitado = event.target.value
+                    if(nomeDigitado.length >= 3) nomeDigitado = nomeDigitado.substring(0,3);
+                    setNome(nomeDigitado)
+                }}
+                id="nome" 
+                label="Nome" 
+                margin="normal" 
+                fullWidth 
+            />
+            <TextField 
+                value={sobrenome}
+                onChange={event => {
+                    setSobrenome(event.target.value)
+                }}
+                id="sobrenome" 
+                label="Sobrenome" 
+                margin="normal" 
+                fullWidth 
+            />
+            <TextField 
+                id="cpf" 
+                label="CPF" 
+                margin="normal" 
+                fullWidth 
+            />
 
-            <label>Sobrenome</label>
-            <input type="text" />
-
-            <label>CPF</label>
-            <input type="text" />
-
-            <label>Promoções</label>
-            <input type="checkbox" />
-
-            <label>Novidades</label>
-            <input type="checkbox" />
+            <FormControlLabel
+                label="Promoções"
+                control={<Switch name="promocoes" defaultChecked color="primary" />}
+            />
+            <FormControlLabel
+                label="Novidades"
+                control={<Switch name="novidades" defaultChecked color="primary" />}
+            />
 
             <Button type="submit" variant="contained">Cadastrar</Button>
         </form>
